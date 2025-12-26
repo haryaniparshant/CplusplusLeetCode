d25d415e180b8951a154195b6061529814aaab07
parshant <parshant4321@gmail.com>
2025-12-26 19:00:50 +0100

    midway

---
+++ Hashmap/Easy/290WordPattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <unordered_map>
+
+using namespace std;
+
+bool wordPattern(string pattern, string s) {
+    return true;
+}
+        
+
+int main() {
+    string s = "abba", t = "dog cat cat fish";
+    cout << wordPattern(s, t) << endl;
+    return 0;
+}
